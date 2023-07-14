4c9e31a905eac5cca5ce34e02748b4d87c548256
syafiqinsanp <112202006504@mhs.dinus.ac.id>
2023-07-14 11:13:31 +0700

    penambahan router

---
+++ src/Components/First.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom"
+
+const First = () => {
+    const navigate = useNavigate()
+
+    return (
+        <>
+        <h3>HOMEPAGE ROUTE</h3>
+        <button onClick={() => navigate('/About')}>About</button>
+        </>
+    )
+    }
+    export default First
