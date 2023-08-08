4dd52a1c9ee86c9c549e1a2fab2b190d2697cf0e
syafiqinsanp <112202006504@mhs.dinus.ac.id>
2023-08-08 14:16:50 +0700

    initial commit

--- src/Components/Profilmitra.jsx
+++ src/Components/Profilmitra.jsx
@@ -4,8 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Profilmitra = () => {
 
-    const [name, setName] = useState('Nama Pengguna');
-    const [description, setDescription] = useState('Deskripsi singkat tentang pengguna');
+    const [name, setName] = useState('');
+    const [alamat, setAlamat] = useState('');
+    const [tgl_lahir, setTgl] = useState('');
+    const [jenis_kel, setKelamin] = useState('');
+    const [email, setEmail] = useState('');
+    const [no_hp, setNomer] = useState('');
     const [editMode, setEditMode] = useState(false);
     const [activeMenu, setActiveMenu] = useState('beranda');
   
@@ -24,7 +28,7 @@ const Profilmitra = () => {
     return (
         <div className="container-news">
           <div className="row" >
-          <nav className="col-md-2 d-none d-md-block sidebar" style={{ backgroundColor: '#0F75BD' }}>
+          <nav className="col-md-2 d-none d-md-block sidebar rounded-4 " style={{ backgroundColor: '#0F75BD' }}>
           <div className="position-sticky">
                 <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-dark">
                 Main Menu
@@ -48,22 +52,29 @@ const Profilmitra = () => {
                 <h1>Profil Akun Anda</h1>
               </div>
               <div className="row">
-                <div className="col-md-4">
-                  <div className="card">
-                    <img
-                      src="https://via.placeholder.com/150"
-                      alt="Profil Pengguna"
-                      className="card-img-top"
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title">{name}</h5>
-                      <p className="card-text">{description}</p>
+                <div className="col-md-3">
+                <div className="card rounded-3">
+                  <img
+                    src="https://via.placeholder.com/150"
+                    alt="Profil Pengguna"
+                    className="card-img-top"
+                  />
+                  <div className="card-body d-flex flex-column align-items-center">
+                      <button className="btn btn-primary me-2 mt-1">
+                        UBAH FOTO PROFIL
+                      </button>
+                      <button className="btn btn-primary me-2 mt-2">
+                        UBAH KATA SANDI
+                      </button>
+                      <button className="btn btn-primary me-2 mt-5">
+                        GANTI AKUN
+                      </button>
                     </div>
                   </div>
                 </div>
                 <div className="col-md-8">
                   <div className="card">
-                    <div className="card-body">
+                    <div className="card-body shadow ps-5 pt-4 rounded-2">
                       {editMode ? (
                         <>
                           <h5 className="card-title">Ubah Profil</h5>
@@ -80,16 +91,64 @@ const Profilmitra = () => {
                             />
                           </div>
                           <div className="mb-3">
-                            <label htmlFor="description" className="form-label">
-                              Deskripsi
+                            <label htmlFor="alamat" className="form-label">
+                              Alamat
                             </label>
                             <textarea
                               className="form-control"
-                              id="description"
+                              id="alamat"
                               rows="3"
-                              value={description}
-                              onChange={(e) => setDescription(e.target.value)}
+                              value={alamat}
+                              onChange={(e) => setAlamat(e.target.value)}
                             ></textarea>
+                          </div>
+                          <div className="mb-3">
+                            <label htmlFor="tgl_lahir" className="form-label">
+                              Tanggal Lahir
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="tgl_lahir"
+                              value={tgl_lahir}
+                              onChange={(e) => setTgl(e.target.value)}
+                            />
+                          </div>
+                          <div className="mb-3">
+                            <label htmlFor="jenis_kel" className="form-label">
+                              Jenis Kelamin
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="jenis_kel"
+                              value={jenis_kel}
+                              onChange={(e) => setKelamin(e.target.value)}
+                            />
+                          </div>
+                          <div className="mb-3">
+                            <label htmlFor="email" className="form-label">
+                              Email Pengguna
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="email"
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}
+                            />
+                          </div>
+                          <div className="mb-3">
+                            <label htmlFor="no_hp" className="form-label">
+                              No. Handphone
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="no_hp"
+                              value={no_hp}
+                              onChange={(e) => setNomer(e.target.value)}
+                            />
                           </div>
                           <button
                             className="btn btn-primary me-2"
@@ -106,9 +165,13 @@ const Profilmitra = () => {
                         </>
                       ) : (
                         <>
-                          <h5 className="card-title">Profil Pengguna</h5>
-                          <p className="card-text">{description}</p>
-                          <button className="btn btn-primary" onClick={handleEdit}>
+                          <h5 className="card-title mt-2">Nama : {name}</h5>
+                          <h5 className="card-title pt-2">Alamat : {alamat}</h5>
+                          <h5 className="card-title pt-2">Tanggal Lahir : {tgl_lahir}</h5>
+                          <h5 className="card-title pt-2">Jenis Kelamin : {jenis_kel}</h5>
+                          <h5 className="card-title pt-2">Email Terkait : {email}</h5>
+                          <h5 className="card-title pt-2 ">No. Handphone : {no_hp}</h5>
+                          <button style={{width: "730px"}} className="btn btn-primary mt-5" onClick={handleEdit}>
                             Edit Profil
                           </button>
                         </>
