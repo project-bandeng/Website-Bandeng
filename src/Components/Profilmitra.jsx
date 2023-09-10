f8ef7b53b01468b6d5d4117af159c3fbf6b455b2
luthfi9251 <90464220+luthfi9251@users.noreply.github.com>
2023-09-10 22:08:43 +0700

    fix foto fi profil mitra

--- src/Components/Profilmitra.jsx
+++ src/Components/Profilmitra.jsx
@@ -1,27 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import axios from '../service/axios';
-import config from '../config';
-import '../App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "../service/axios";
+import config from "../config";
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import useBackendURLTranslator from "../hooks/useBackendURLTranslator";
 
 // TO DO :
 // TAMBAHIN LOADING
 // TAMBAHIN NOTIFIKASI BERHASIL UPDATE
 
 const Profilmitra = () => {
-  document.title = "Profil Mitra"; 
-  const [name, setName] = useState('');
-  const [alamat, setAlamat] = useState('');
-  const [tgl_lahir, setTgl] = useState('');
-  const [jenis_kel, setKelamin] = useState('');
-  const [email, setEmail] = useState('');
-  const [no_hp, setNomer] = useState('');
-  const [fotoMitra, setFotoMitra] = useState('https://via.placeholder.com/150');
+  document.title = "Profil Mitra";
+  const [name, setName] = useState("");
+  const [alamat, setAlamat] = useState("");
+  const [tgl_lahir, setTgl] = useState("");
+  const [jenis_kel, setKelamin] = useState("");
+  const [email, setEmail] = useState("");
+  const [no_hp, setNomer] = useState("");
+  const [fotoMitra, setFotoMitra] = useState("https://via.placeholder.com/150");
   const [file, setFile] = useState(null);
   const [editMode, setEditMode] = useState(false);
-  const [activeMenu, setActiveMenu] = useState('profil');
+  const [activeMenu, setActiveMenu] = useState("profil");
   const navigate = useNavigate();
+  const convertImage = useBackendURLTranslator();
 
   const handleEdit = () => {
     setEditMode(true);
@@ -31,7 +33,7 @@ const Profilmitra = () => {
     console.log(e.target.files[0]);
     if (e.target.files[0].size > 5024000) {
       //TODO : GANTI PAKE SWEETALERT
-      alert('File tidak boleh lebih besar dari 5MB!');
+      alert("File tidak boleh lebih besar dari 5MB!");
       return;
     }
     setFile(e.target.files[0]);
@@ -39,14 +41,14 @@ const Profilmitra = () => {
   };
 
   const handleSavePhoto = () => {
-    if (!file) return console.log('Tidak ada foto');
+    if (!file) return console.log("Tidak ada foto");
     const formData = new FormData();
     let id = getIDFromLocalStorage();
-    formData.append('foto_mitra', file);
+    formData.append("foto_mitra", file);
     axios
       .post(`/api/v1/mitra/edit-foto/${id}`, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
+          "Content-Type": "multipart/form-data",
         },
       })
       .then((res) => {
@@ -89,30 +91,30 @@ const Profilmitra = () => {
   };
 
   function getIDFromLocalStorage() {
-    return JSON.parse(localStorage.getItem('data-user'))?.id;
+    return JSON.parse(localStorage.getItem("data-user"))?.id;
   }
 
   function setPhotoFromResponse(url) {
     if (!url) {
-      setFotoMitra('https://via.placeholder.com/150');
+      setFotoMitra("https://via.placeholder.com/150");
       return;
     }
 
     //DIGANTI SESUAL ALAMAT BACKEND
-    const BACKEND_DOMAIN = config.BACKEND_URL;
-    let path = BACKEND_DOMAIN + url;
-    setFotoMitra(path);
-    console.log(path);
+    // const BACKEND_DOMAIN = config.BACKEND_URL;
+    // let path = BACKEND_DOMAIN + url;
+    setFotoMitra(convertImage(url));
+    // console.log(path);
   }
 
   const handleLogout = () => {
     axios
-      .get('/api/v2/logout-mitra')
+      .get("/api/v2/logout-mitra")
       .then((res) => {
         console.log(res);
-        localStorage.removeItem('data-user');
-        localStorage.removeItem('auth-token');
-        navigate('/login');
+        localStorage.removeItem("data-user");
+        localStorage.removeItem("auth-token");
+        navigate("/login");
       })
       .catch((err) => {
         console.log(err);
@@ -120,8 +122,8 @@ const Profilmitra = () => {
   };
 
   useEffect(() => {
-    if (!localStorage.getItem('auth-token')) {
-      return navigate('/login');
+    if (!localStorage.getItem("auth-token")) {
+      return navigate("/login");
     }
     let idUser = getIDFromLocalStorage();
     axios
@@ -130,13 +132,13 @@ const Profilmitra = () => {
         //set State berdasarkan response dari server
         let data = res.data.data;
         console.log(data);
-        setName(data.namaLengkap || '');
-        setAlamat(data.alamatMitra || '');
-        setTgl(data.tglLahir || '');
-        setKelamin(data.jeniskel || '');
-        setEmail(data.email || '');
-        setNomer(data.no_tlp || '');
-        setPhotoFromResponse(data['foto_mitra']);
+        setName(data.namaLengkap || "");
+        setAlamat(data.alamatMitra || "");
+        setTgl(data.tglLahir || "");
+        setKelamin(data.jeniskel || "");
+        setEmail(data.email || "");
+        setNomer(data.no_tlp || "");
+        setPhotoFromResponse(data["foto_mitra"]);
       })
       .catch((err) => {
         console.log(err);
@@ -146,27 +148,56 @@ const Profilmitra = () => {
   return (
     <div className="container-news">
       <div className="row">
-      <nav className="col-md-2 d-none d-md-block sidebar rounded-4 pe-0 ps-4" style={{ backgroundColor: '#0F75BD' }}>
+        <nav
+          className="col-md-2 d-none d-md-block sidebar rounded-4 pe-0 ps-4"
+          style={{ backgroundColor: "#0F75BD" }}
+        >
           <div className="d-flex flex-column justify-content-between h-100">
-          <div className="position-sticky">
-            <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-white">Main Menu</h5>
-            <ul className="nav flex-column gap-2">
-              <li className={`nav-item rounded-start-3 ${activeMenu === 'profil' ? 'active' : ''}`}>
-                <a className={`nav-link text-primary  ${activeMenu === 'profil' ? 'active-link' : '/profil'}`} href="/profil" onClick={() => setActiveMenu('profil')}>
-                  <i class="bi bi-person-fill me-2"></i>
-                  PROFILE
-                </a>
-              </li>
-              <li className={`nav-item ${activeMenu === 'products' ? 'active' : ''}`}>
-                <a className={`nav-link text-white hovering-menu rounded-start-3 ${activeMenu === 'products' ? 'active-link' : '/crudproduk'}`} href="/crudproduk" onClick={() => setActiveMenu('products')}>
-                  <i class="bi bi-bag-fill me-2"></i>
-                  PRODUCTS
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="logout mt-auto mb-3">
-              <Link className={`nav-link text-white nav-item px-3 py-2 hovering-menu rounded-start-3`} onClick={() => handleLogout()} style={{ cursor: 'pointer' }}>
+            <div className="position-sticky">
+              <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-white">
+                Main Menu
+              </h5>
+              <ul className="nav flex-column gap-2">
+                <li
+                  className={`nav-item rounded-start-3 ${
+                    activeMenu === "profil" ? "active" : ""
+                  }`}
+                >
+                  <a
+                    className={`nav-link text-primary  ${
+                      activeMenu === "profil" ? "active-link" : "/profil"
+                    }`}
+                    href="/profil"
+                    onClick={() => setActiveMenu("profil")}
+                  >
+                    <i class="bi bi-person-fill me-2"></i>
+                    PROFILE
+                  </a>
+                </li>
+                <li
+                  className={`nav-item ${
+                    activeMenu === "products" ? "active" : ""
+                  }`}
+                >
+                  <a
+                    className={`nav-link text-white hovering-menu rounded-start-3 ${
+                      activeMenu === "products" ? "active-link" : "/crudproduk"
+                    }`}
+                    href="/crudproduk"
+                    onClick={() => setActiveMenu("products")}
+                  >
+                    <i class="bi bi-bag-fill me-2"></i>
+                    PRODUCTS
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="logout mt-auto mb-3">
+              <Link
+                className={`nav-link text-white nav-item px-3 py-2 hovering-menu rounded-start-3`}
+                onClick={() => handleLogout()}
+                style={{ cursor: "pointer" }}
+              >
                 <i class="bi bi-door-open-fill me-2"></i>
                 LOGOUT
               </Link>
@@ -180,13 +211,29 @@ const Profilmitra = () => {
           <div className="row">
             <div className="col-md-3">
               <div className="card rounded-3">
-                <img src={fotoMitra} alt="Profil Pengguna" className="card-img-top" />
+                <img
+                  src={fotoMitra}
+                  alt="Profil Pengguna"
+                  className="card-img-top"
+                />
                 <div className="card-body d-flex flex-column align-items-center">
-                  <label className="btn btn-primary me-2 mt-1" htmlFor="fotoprofil">
+                  <label
+                    className="btn btn-primary me-2 mt-1"
+                    htmlFor="fotoprofil"
+                  >
                     UBAH FOTO PROFIL
                   </label>
-                  <input type="file" name="fotoprofil" id="fotoprofil" className="visually-hidden" accept="image/*" onChange={handleOnChangeFoto} />
-                  <button className="btn btn-primary me-2 mt-2">UBAH KATA SANDI</button>
+                  <input
+                    type="file"
+                    name="fotoprofil"
+                    id="fotoprofil"
+                    className="visually-hidden"
+                    accept="image/*"
+                    onChange={handleOnChangeFoto}
+                  />
+                  <button className="btn btn-primary me-2 mt-2">
+                    UBAH KATA SANDI
+                  </button>
                 </div>
               </div>
             </div>
@@ -200,54 +247,108 @@ const Profilmitra = () => {
                         <label htmlFor="name" className="form-label">
                           Nama Pengguna
                         </label>
-                        <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="name"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                        />
                       </div>
                       <div className="mb-3">
                         <label htmlFor="alamat" className="form-label">
                           Alamat
                         </label>
-                        <textarea className="form-control" id="alamat" rows="3" value={alamat} onChange={(e) => setAlamat(e.target.value)}></textarea>
+                        <textarea
+                          className="form-control"
+                          id="alamat"
+                          rows="3"
+                          value={alamat}
+                          onChange={(e) => setAlamat(e.target.value)}
+                        ></textarea>
                       </div>
                       <div className="mb-3">
                         <label htmlFor="tgl_lahir" className="form-label">
                           Tanggal Lahir
                         </label>
-                        <input type="date" className="form-control" id="tgl_lahir" value={tgl_lahir} onChange={(e) => setTgl(e.target.value)} />
+                        <input
+                          type="date"
+                          className="form-control"
+                          id="tgl_lahir"
+                          value={tgl_lahir}
+                          onChange={(e) => setTgl(e.target.value)}
+                        />
                       </div>
                       <div className="mb-3">
                         <label htmlFor="jenis_kel" className="form-label">
                           Jenis Kelamin
                         </label>
-                        <input type="text" className="form-control" id="jenis_kel" value={jenis_kel} onChange={(e) => setKelamin(e.target.value)} />
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="jenis_kel"
+                          value={jenis_kel}
+                          onChange={(e) => setKelamin(e.target.value)}
+                        />
                       </div>
                       <div className="mb-3">
                         <label htmlFor="email" className="form-label">
                           Email Pengguna
                         </label>
-                        <input disabled type="text" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input
+                          disabled
+                          type="text"
+                          className="form-control"
+                          id="email"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
                       </div>
                       <div className="mb-3">
                         <label htmlFor="no_hp" className="form-label">
                           No. Handphone
                         </label>
-                        <input type="text" className="form-control" id="no_hp" value={no_hp} onChange={(e) => setNomer(e.target.value)} />
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="no_hp"
+                          value={no_hp}
+                          onChange={(e) => setNomer(e.target.value)}
+                        />
                       </div>
-                      <button className="btn btn-primary me-2" onClick={handleSave}>
+                      <button
+                        className="btn btn-primary me-2"
+                        onClick={handleSave}
+                      >
                         Simpan
                       </button>
-                      <button className="btn btn-secondary" onClick={handleCancel}>
+                      <button
+                        className="btn btn-secondary"
+                        onClick={handleCancel}
+                      >
                         Batal
                       </button>
                     </>
                   ) : (
-                    <div className='d-flex flex-column'>
+                    <div className="d-flex flex-column">
                       <h5 className="card-title mt-2">Nama : {name}</h5>
                       <h5 className="card-title pt-2">Alamat : {alamat}</h5>
-                      <h5 className="card-title pt-2">Tanggal Lahir : {tgl_lahir}</h5>
-                      <h5 className="card-title pt-2">Jenis Kelamin : {jenis_kel}</h5>
-                      <h5 className="card-title pt-2">Email Terkait : {email}</h5>
-                      <h5 className="card-title pt-2 ">No. Handphone : {no_hp}</h5>
-                      <button className="btn btn-primary mt-4 align-self-center" onClick={handleEdit}>
+                      <h5 className="card-title pt-2">
+                        Tanggal Lahir : {tgl_lahir}
+                      </h5>
+                      <h5 className="card-title pt-2">
+                        Jenis Kelamin : {jenis_kel}
+                      </h5>
+                      <h5 className="card-title pt-2">
+                        Email Terkait : {email}
+                      </h5>
+                      <h5 className="card-title pt-2 ">
+                        No. Handphone : {no_hp}
+                      </h5>
+                      <button
+                        className="btn btn-primary mt-4 align-self-center"
+                        onClick={handleEdit}
+                      >
                         Edit Profil
                       </button>
                     </div>
